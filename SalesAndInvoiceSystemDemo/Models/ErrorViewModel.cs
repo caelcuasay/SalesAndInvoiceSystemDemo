b6031283fb8aaa7b8f061e2a1fbe70ac67105463
LENOVO <LENOVO@LAPTOP-5PFC0V1I>
2026-02-09 09:14:09 +0800

    Add project files.

---
+++ SalesAndInvoiceSystemDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesAndInvoiceSystemDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
